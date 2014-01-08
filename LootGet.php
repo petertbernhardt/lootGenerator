3d617574117437f6d43302720d5714f13d571f4a
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2014-01-07 20:48:58 -0800

    MySQL Credentials added
    Added MySQL connection details to LootGet.php.
    Fixed a typo in the <form> tag in LootGet2.html.

--- LootGet.php
+++ LootGet.php
@@ -1,10 +1,9 @@
 <?php
-	// $mysqli_connection = new MySQLi('address', 'username', 'password', 'databasename', portnum);
-	/* if($mysqli_connection->connect_error){
+	$mysqli_connection = new MySQLi('localhost', 'peterb22_itemGen', 'bD2g^gS^S!BQ', 'peterb22_itemGenerator');
+	if($mysqli_connection->connect_error){
 		echo "Not connected, error: ".$mysqli_connection->connect_error;
 	} else {
 		echo "Connected.";
 	}
-	*/
 	var_dump($_GET);
 ?>
