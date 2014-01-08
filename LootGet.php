be894f504a1cf0931d3d5e630fe14dbcb1c41ac8
Peter Thomas Bernhardt <petertbernhardt@gmail.com>
2014-01-07 21:08:58 -0800

    Comments in LootGet.php
    Added comments to LootGet.php.
    Closed the MySQL connection at the end of the file.

--- LootGet.php
+++ LootGet.php
@@ -1,9 +1,14 @@
 <?php
+	// Connect to DB
 	$mysqli_connection = new MySQLi('localhost', 'peterb22_itemGen', 'bD2g^gS^S!BQ', 'peterb22_itemGenerator');
+	// Check the connection
 	if($mysqli_connection->connect_error){
 		echo "Not connected, error: ".$mysqli_connection->connect_error;
 	} else {
 		echo "Connected.";
 	}
+	// Print out all post variables
 	var_dump($_GET);
+	
+	mysqli_close($mysqli_connection);
 ?>
